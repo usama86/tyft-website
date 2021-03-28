a882076a23b3793d1234000f7b8ba224d0649754
Usama <usama.amjad32@gmail.com>
2021-03-29 01:45:45 +0500

    s
    s

--- src/AdminPanel/ServingCusine.js
+++ src/AdminPanel/ServingCusine.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Label from './Ui/Label';
 
+const useStyles = makeStyles((theme) => ({
+	root: {
+		width: '100%',
+		maxWidth: 360,
+		backgroundColor: theme.palette.background.paper,
+        maxHeight: '600px',
+        overflow: 'scroll',
+        overflowX: 'hidden'
+	}
+}));
 const ServingCusine = (props) => {
+	const classes = useStyles();
 	let { cusines } = props;
-	return <div>{cusines ? cusines.map((data) => 
-    <div>{data.cusineName}</div>
-    
-    ) : null}</div>;
+	return (
+        <React.Fragment>
+            <Label style={{
+                fontSize: '20px',
+                fontWeight: 'bold',
+                margin: '100px',
+            }}>Serving Cusines</Label>
+        <List component="nav" className={classes.root} aria-label="contacts">
+			{cusines ? (
+				cusines.map((data) => (
+				
+						<ListItem button>
+							<ListItemText inset primary={data.cusineName} />
+						</ListItem>
+					
+				))
+			) : null}
+		</List>
+        </React.Fragment>
+	);
 };
 export default ServingCusine;
